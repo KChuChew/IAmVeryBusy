25669ca1de7030c95f4cdcd61645febac2acb05e
Kevin Chu - KCKH <kevin.chu-kckh@hermes-microvision.com>
2019-08-06 13:56:49 -0700

    Initial Script moves mouse left right constant Y axis

---
+++ MouseMove.ps1
@@ -0,0 +1,23 @@
+﻿param($minutes = 60)
+
+$myshell = New-Object -com "Wscript.Shell"
+
+$moveLeft = $true;
+$Pos = [System.Windows.Forms.Cursor]::Position
+
+while($true) 
+{    
+    
+    If($moveLeft) 
+    {
+        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) - 100) , $Pos.Y)
+        $moveLeft = $false
+    }
+    Else 
+    {
+        [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) + 100) , $Pos.Y)
+        $moveLeft = $true
+    }
+    
+    Start-Sleep -Seconds 30
+}
